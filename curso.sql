ac94af2e9211ded9fa51dbb1a9b632b500cdbed6
trolcito76 <67081436+trolcito76@users.noreply.github.com>
2020-07-21 12:24:28 -0400

    Delete curso.sql

--- curso.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
--- Archivo creado  - martes-julio-21-2020   
---------------------------------------------------------
-REM INSERTING into EXFINAL.CURSO
-SET DEFINE OFF;
-Insert into EXFINAL.CURSO (ID_PROG,PROGRAMA) values ('Android','Android');
-Insert into EXFINAL.CURSO (ID_PROG,PROGRAMA) values ('Fullstack','Java');
-Insert into EXFINAL.CURSO (ID_PROG,PROGRAMA) values ('BD','Access');
