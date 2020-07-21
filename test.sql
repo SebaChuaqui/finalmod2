c19d48a7592b27653c2effa461356b8503c9f291
trolcito76 <67081436+trolcito76@users.noreply.github.com>
2020-07-21 12:29:50 -0400

    Carga de Datos en Tabla

---
+++ test.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+-- Archivo creado  - martes-julio-21-2020   
+--------------------------------------------------------
+REM INSERTING into EXFINAL.TEST
+SET DEFINE OFF;
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T01','Test 01','Test difícil','Android','Mod01','SCHUAQUI',to_date('17/07/20','DD/MM/RR'),'Android');
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T02','Test 02','Test coplejo','Java','Mod01','SCHUAQUI',to_date('17/06/20','DD/MM/RR'),'Fullstack');
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T03','Test 03','Test fácil','Android','Mod01','SCHUAQUI',to_date('17/05/20','DD/MM/RR'),'Android');
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T04','Test 04','Test intermedio','Java','Mod01','SCHUAQUI',to_date('17/04/20','DD/MM/RR'),'Fullstack');
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T05','Test 05','Test chanta','Android','Mod01','SCHUAQUI',to_date('17/03/20','DD/MM/RR'),'Android');
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T06','Test 06','Test experto','Java','Mod01','SCHUAQUI',to_date('17/02/20','DD/MM/RR'),'Fullstack');
+Insert into EXFINAL.TEST (ID_TEST,NOMBRE,DESCRIPCION,PROGRAMA,UNIDAD,AUTOR,FEC_CREAC,CURSO_ID_PROG) values ('T07','Test 07','Test nivel dios','Android','Mod01','SCHUAQUI',to_date('17/01/20','DD/MM/RR'),'Android');
