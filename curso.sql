d24df0b4817f64043c99dac6716b8eb9c1a133e2
trolcito76 <67081436+trolcito76@users.noreply.github.com>
2020-07-21 12:19:53 -0400

    Add files via upload
    Inserción de datos en las tablas

---
+++ curso.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+-- Archivo creado  - martes-julio-21-2020   
+--------------------------------------------------------
+REM INSERTING into EXFINAL.CURSO
+SET DEFINE OFF;
+Insert into EXFINAL.CURSO (ID_PROG,PROGRAMA) values ('Android','Android');
+Insert into EXFINAL.CURSO (ID_PROG,PROGRAMA) values ('Fullstack','Java');
+Insert into EXFINAL.CURSO (ID_PROG,PROGRAMA) values ('BD','Access');
---
+++ preguntas.sql
@@ -0,0 +1,35 @@
+--------------------------------------------------------
+-- Archivo creado  - martes-julio-21-2020   
+--------------------------------------------------------
+REM INSERTING into EXFINAL.PREGUNTAS
+SET DEFINE OFF;
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P01','¿Cuál es la sentencia para crear un ciclo FOR en PSEINT?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P02','¿Cuál sentencia se aplica en Java para colocar una condición?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P03','¿Cómo se llama el programa para modelar datos?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P04','¿Cómo se llama el profe que nos aprueba el examen?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P05','¿Qué es una tabla?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P06','¿Qué es un campo?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P07','¿Qué es una validación?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P08','¿Para que sirve el "{" y "}" en Java?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P09','¿Qué es un "Integer"?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P10','¿Cuántos valores puedes ingresar en un VARCHAR(20)?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P11','¿Qué es un "Boolean"?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P12','¿Se puede programar una cuarentena con conflicto social?','100','T01');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P13','¿Qué es un "Script"?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P14','¿En qué idioma opera SQL Developer?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P15','¿Qué es un dicionario de datos?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P16','¿Qué es un XML?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P17','¿Qué es un DDM?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P18','¿Qué es un DDL?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P19','¿Qué hace "INSERT INTO" en SQL Developer?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P20','¿Cómo se escribe una consulta?','100','T02');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P21','¿Se puede crear un objeto "private" en Java?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P22','¿Cuantas tablas se puede crear en un programa?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P23','¿La sentencia "Create Beer Into Select" entrega...?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P24','¿La sentencia R2D2 genera...?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P25','¿Qué es un LOG?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P26','¿Java de Hutt sirve para...?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P27','¿Awakelab despierta tu mente?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P28','¿En PSEINT "While" equivale a...?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P29','¿El mejor curso de Awakelab es...?','100','T03');
+Insert into EXFINAL.PREGUNTAS (ID_PRE,TEXTO,PUNTAJE,TEST_ID_TEST) values ('P30','¿SQL Developer Data Modeler da problemas a Juan Molina...?','100','T03');
